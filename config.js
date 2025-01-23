bcb864496da12f7eaabd7582df5f061c0aaada36
Nadeenpoorna-lk <nadeenpoorna058@gmail.com>
2025-01-23 21:32:45 +0530

    💥NADEEN-MD💥

---
+++ config.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "", //put your session id
+MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
+MODE: process.env.MODE || "public", //private or public
+AUTO_VOICE:"true" //true or false
+};
