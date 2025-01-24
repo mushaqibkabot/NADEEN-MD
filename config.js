166bd0dd8d3aa6a65c1d7e2c1c2444c78fa8aafa
Nadeenpoorna-lk <nadeenpoorna058@gmail.com>
2025-01-24 17:30:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,10 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "public", //private or public
+STATUES_REPLY: process.env.STATUES_REPLY || "true",
+AUTO_REACT: process.env.AUTO_REACT || "true",
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
+DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '94711451319' : process.env.DELETEMSGSENDTO, //ADD YOUR NUMBER
+INBOX_BLOCK: process.env.INBOX_BLOCK || "false",
 AUTO_VOICE:"true" //true or false
 };
